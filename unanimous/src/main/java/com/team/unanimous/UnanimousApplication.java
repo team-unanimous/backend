8fb7350474b7530ad65c1a5e6327797c8001fbbb
DKKwag <105155862+DKKwag@users.noreply.github.com>
2022-06-25 14:25:13 +0900

    feat: 프로젝트 생성

---
+++ unanimous/src/main/java/com/team/unanimous/UnanimousApplication.java
@@ -0,0 +1,13 @@
+package com.team.unanimous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnanimousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnanimousApplication.class, args);
+    }
+
+}
---
+++ unanimous/src/test/java/com/team/unanimous/UnanimousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.unanimous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnanimousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
