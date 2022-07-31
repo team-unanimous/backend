eafad0f5f8191b4735453e278e31c74ab382c916
DKKwag <105155862+DKKwag@users.noreply.github.com>
2022-08-01 07:00:44 +0900

    Update run_new_was.sh

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -22,7 +22,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-sudo chmod -R 777 /home/ubuntu/unanimous
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/unanimous/unanimous/build/libs/unanimous-0.0.1-SNAPSHOT.jar > /home/ubuntu/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/unanimous/unanimous/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
